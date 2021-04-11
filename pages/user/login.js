a79ef6eaf4ed262b27bc167adf1ae0d929a72cbd
DJWL <1602951127@qq.com>
2021-04-11 16:17:28 +0800

    update

--- pages/user/login.js
+++ pages/user/login.js
@@ -37,7 +37,6 @@ class Login extends React.Component {
         })
         .then((res) => {
           if (res != '') {
-            // console.log(res[0])
             const re = res[0];
             storage
               .save({
